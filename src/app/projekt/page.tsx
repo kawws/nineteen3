d31324a75a6fe8d0005dda6c0a4ecf3d746fa939
Eddie <101603227+eddie69420@users.noreply.github.com>
2024-12-29 20:07:35 +0100

    Added new feature or fixed a bug

--- src/app/projekt/page.tsx
+++ src/app/projekt/page.tsx
@@ -5,52 +5,69 @@ export default function ProjectPage() {
         <div className="grid-container">
             <div className="grid-row">
               <div className="grid-item">
-                <img src="projektbilder/tjuvjakt-gra.jpeg" alt="projekt1"/>
+                <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/tjuvjakt-1.jpeg" alt="Tjuvjakt"/>
               </div>
               <div className="grid-item">
-                <img src="projektbilder/tjuvjakt-svart.jpeg" alt=""/>
+                <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/tjuvjakt-2.jpeg" alt="Tjuvjakt"/>
               </div>
             </div>
             <div className="grid-row">
                 <div className="grid-item">
-                    <img src="projektbilder/stora-creator-bak.PNG" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/clean-1.jpeg" alt="Clean Drinks"/>
                 </div>
                 <div className="grid-item">
-                    <img src="projektbilder/stora-sparker-bak.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/clean-2.jpeg" alt="Clean Drinks"/>
                 </div>
                 <div className="grid-item">
-                    <img src="projektbilder/sparker-vit.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/clean-3.jpeg" alt="Clean Drinks"/>
                 </div>
             </div>
             <div className="grid-row">
                 <div className="grid-item">
-                    <img src="projektbilder/menomale-ig.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/pim-1.png" alt="Pim Studios"/>
                 </div>
                 <div className="grid-item">
-                    <img src="projektbilder/intrum-oppnare.jpeg" alt=""/>
-                </div>
-            </div>
-            <div className="grid-row">
-              
-                <div className="grid-item">
-                    <img src="projektbilder/wall-hoodie-ig.jpeg" alt=""/>
-                </div>
-                <div className="grid-item">
-                    <img src="projektbilder/wall-tee-ig.jpeg" alt=""/>
-                </div>
-                <div className="grid-item">
-                    <img src="projektbilder/wall-lighter-ig.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/pim-2.jpeg" alt="Pim studios"/>
                 </div>
             </div>
             <div className="grid-row">
                 <div className="grid-item">
-                    <img src="projektbilder/vnervf.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/stora-bageriet-1.jpeg" alt="Stora Bageriet"/>
                 </div>
                 <div className="grid-item">
-                    <img src="projektbilder/cejhbrjvh.jpeg" alt=""/>
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/stora-bageriet-2.jpeg" alt="Stora Bageriet"/>
+                </div>
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/stora-bageriet-3.jpeg" alt="Stora Bageriet"/>
                 </div>
             </div>
-
+            <div className="grid-row">
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/intrum.jpeg" alt="Intrum"/>
+                </div>
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/menomale.jpeg" alt="Menomale"/>
+                </div>
+            </div>
+            <div className="grid-row">
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/wall-1.jpeg" alt="Wall Nightclub"/>
+                </div>
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/wall-2.jpeg" alt="Wall Nightclub"/>
+                </div>
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/wall-3.jpeg" alt="Wall Nightclub"/>
+                </div>
+            </div>
+            <div className="grid-row">
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/harkommerlyckan-bild.jpeg" alt="Totebag Med Tryck"/>
+                </div>
+                <div className="grid-item">
+                    <img src="https://pub-3635b466c9684646a3ba1cf1ed544c70.r2.dev/pass-me-the-wine.jpeg" alt="Pass Me The Wine"/>
+                </div>
+            </div>
           </div>
       </>
     );
